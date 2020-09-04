436a9499be2c0bdeac4c8da64568b914b41b19f0
solbi <solbi5555@gmail.com>
2020-09-04 23:17:12 +0900

    weather component 추가, condition 정리

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Alert} from 'react-native'
 import Loading from './Loading'
+import Weather from './Weather'
 import * as Location from 'expo-location';
 import axios from 'axios'
 
@@ -12,9 +13,18 @@ export default class extends React.Component  {
     isLoading: true
   }
   getWeather = async (latitude,longitude) => {
-    const {data} = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    const { 
+      data: {
+        main : {temp},
+        weather
+      }
+    } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
 
-    console.log('data',data)
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp, 
+    })
   }
 
  getLocation = async () => {
@@ -23,7 +33,6 @@ export default class extends React.Component  {
     const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync()
     //send to API and get weather
     this.getWeather(latitude,longitude)
-    this.setState({isLoading: false})
    } catch (error) {
     Alert.alert("Can't find you.")
   }
@@ -34,8 +43,8 @@ componentDidMount() {
 }
 
 render() {
-  const {isLoading} = this.state
-    return isLoading ? <Loading/> : null
+  const {isLoading,temp,condition} = this.state
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>
   }
 }
 
